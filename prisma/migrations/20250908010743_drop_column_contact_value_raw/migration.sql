fa764a8fb486d1f1335971aee1fc05f5fa9e21b9
imooje <savvyplus020@gmail.com>
2025-09-08 02:57:15 +0100

    manually update migration

--- prisma/migrations/20250908010743_drop_column_contact_value_raw/migration.sql
+++ prisma/migrations/20250908010743_drop_column_contact_value_raw/migration.sql
@@ -4,8 +4,6 @@
   - You are about to drop the column `contactValueRaw` on the `Contact` table. All the data in the column will be lost.
 
 */
--- DropIndex
-DROP INDEX "public"."Contact_contactValueRaw_key";
 
 -- AlterTable
 ALTER TABLE "public"."Contact" DROP COLUMN "contactValueRaw";
